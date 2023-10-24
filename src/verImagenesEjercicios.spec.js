8ad3b4ad63af544903cf158d61df1a8868ccb929
987sebasBeller <sebastianbeller76@gmail.com>
2023-10-24 15:10:04 -0400

    Refactoring pruebas Ver Imagen Ejercicios

--- src/verImagenesEjercicios.spec.js
+++ src/verImagenesEjercicios.spec.js
@@ -1,13 +1,15 @@
 import { Ejercicio } from "./Ejercicio";
 import * as constantes from "./constantes";
+function getImagenEjercicio(titulo,categoria,imagen){
+    let ejercicio= new Ejercicio(titulo,categoria,imagen);
+    return ejercicio.getImagen();
+}
 describe("Ver imagen de ejercicio", () => {
       it("Devuelve imagen predeterminada de un ejercicio", () => {
-        let ejercicio= new Ejercicio("Numeros Primos","Numeros");
-        expect(ejercicio.getImagen()).toEqual(constantes.IMAGENPREDETERMINADA);
+        expect(getImagenEjercicio("Numeros Primos","Numeros")).toEqual(constantes.IMAGENPREDETERMINADA);
       });
       it("Devuelve imagen propia del ejercicio", () => {
-        let ejercicio= new Ejercicio("Numeros Romanos","Numeros","10PUZY7ai9akJ5VAGzWbPNKGjQXBQ4-f9");
-        expect(ejercicio.getImagen()).toEqual("10PUZY7ai9akJ5VAGzWbPNKGjQXBQ4-f9");
+        expect(getImagenEjercicio("Numeros Romanos","Numeros","10PUZY7ai9akJ5VAGzWbPNKGjQXBQ4-f9")).toEqual("10PUZY7ai9akJ5VAGzWbPNKGjQXBQ4-f9");
       });
       
     }
