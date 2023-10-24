2930dab91f8449f6cb7d0c7625c599a5dc1a91a4
Wama85 <wi_an9@hotmail.com>
2023-10-23 23:48:20 -0400

    Crear un ejercicio en el catálogo

--- src/presenter.js
+++ src/presenter.js
@@ -1,43 +1,20 @@
 import { Catalogo } from "./Catalogo.js";
 import { Ejercicio } from "./Ejercicio.js";
 import { ejercicios } from "./dataCatalogo.js";
-const catalogo= new Catalogo(ejercicios);
 const detalleCatalogo=document.getElementById("detallecatalogo");
-const menucatalogo=document.getElementById("menucatalogo");
-const menuacregarejer=document.getElementById("menuacregarejer");
-const contenidocatalogo=document.getElementById("contenidocatalogo");
-const formularioejercicio=document.getElementById("formularioejercicio");
-const btnregistrar=document.getElementById("crearregistro");
-formularioejercicio.style.display="none";
-
-menucatalogo.addEventListener("click", (env)=>{
-  
-  env.preventDefault();
-  formularioejercicio.style.display="none";
-  contenidocatalogo.style.display="block";
-  llenarEjercicios();
- 
-});
-menuacregarejer.addEventListener("click", (env)=>{
-  env.preventDefault();
-  contenidocatalogo.style.display="none";
-  formularioejercicio.style.display="block";
- 
-})
-btnregistrar.addEventListener("click", (env)=>{
-  env.preventDefault();
-   crearjercicio();
-})
-   function llenarEjercicios(){
-    detalleCatalogo.innerHTML="";
-    let ejerciciosHTML=``;
-    console.log(catalogo)
-    if(catalogo.verificarListaVacia()=="No se tiene ejercicios disponibles" || detalleCatalogo==null){
-      detalleCatalogo.innerHTML+=`<p>${catalogo.verificarListaVacia()}</p>`;
-      return;
-    }
-    catalogo.getEjercicios().forEach(ejercicio => {
-        ejerciciosHTML+=`
+const catalogo= new Catalogo(ejercicios);
+function llenarEjercicios() {
+  detalleCatalogo.innerHTML = "";
+  let ejerciciosHTML = ``;
+  console.log(catalogo);
+  if (catalogo.verificarListaVacia() == "No se tiene ejercicios disponibles" ||
+    detalleCatalogo == null)
+  {
+    detalleCatalogo.innerHTML += `<p>${catalogo.verificarListaVacia()}</p>`;
+    return;
+  }
+  catalogo.getEjercicios().forEach((ejercicio) => {
+    ejerciciosHTML += `
         <div id="imgdetallecatalog">
           <div id="marcoimagen">
             <img src="${require("./img/predeterminado.png")}" width="150px" height="auto">
@@ -47,31 +24,26 @@ btnregistrar.addEventListener("click", (env)=>{
           <h3>${ejercicio.getTitulo()}</h3>
           <p>${ejercicio.getResumen()}<span>Ver mas</span></p>
         </div>`;
-    });
-    detalleCatalogo.innerHTML+=ejerciciosHTML;
+  });
+  detalleCatalogo.innerHTML += ejerciciosHTML;
+}
+function crearjercicio() {
+  const txtNombre = document.getElementById("txtNombre");
+  const selecCategoria = document.getElementById("selecCategoria");
+  const txtDetalle = document.getElementById("txtDetalle");
+
+  const nombre = txtNombre.value;
+  const categoria = selecCategoria.value;
+  const detalle = txtDetalle.value;
+
+  let ejercicio = new Ejercicio(nombre, categoria, undefined, detalle);
+
+  if (ejercicio.validarAtributos() == true) {
+    alert("Datos subidos correctamente");
+    catalogo.agregarEjercicio(ejercicio);
+    llenarEjercicios();
+  } else {
+    alert("Todos los campos son obligatorios");
   }
- function crearjercicio(){
-const txtNombre=document.getElementById("txtNombre");
-const selecCategoria=document.getElementById("selecCategoria");
-const txtDetalle=document.getElementById("txtDetalle");
-
-    const nombre=txtNombre.value;
-    const categoria=selecCategoria.value;
-    const detalle=txtDetalle.value;
-      
-    let ejercicio=new Ejercicio(nombre,categoria,undefined,detalle);
-   
-    if(ejercicio.validarAtributos()==true){
-     alert("Datos subidos correctamente")
-     catalogo.agregarEjercicio(ejercicio);
-     llenarEjercicios();
-        }
-    else{
-      alert("Todos los campos son obligatorios")
-    }
- }
-  
-
- llenarEjercicios();
-
-
+}
+export { llenarEjercicios ,crearjercicio};
