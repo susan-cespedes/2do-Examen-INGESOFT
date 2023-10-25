258c57d6f3c26e2a2eec77e09ab8e889b22fd28f
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-10-24 20:58:53 -0400

    Refactoring pruebas buscarPorNombre

--- src/buscarPorNombre.spec.js
+++ src/buscarPorNombre.spec.js
@@ -1,29 +1,29 @@
 import { Ejercicio } from "./Ejercicio";
 import { Catalogo } from "./Catalogo";
 import { ejerciciosPruebas } from "./constantes";
+
+function buscarPorNombre(ejercicios,nombreBuscado){
+  let catalogo=new Catalogo(ejercicios);
+  return catalogo.buscarPorNombre(nombreBuscado);
+}
+
 describe("Buscar ejercicio por nombre", () => {
     it("Devuelve vacio no encontro el ejercicio", () => {
-        let catalogo=new Catalogo(ejerciciosPruebas);
-        expect(catalogo.buscarPorNombre("Numeros Random")).toEqual([]);
+        expect(buscarPorNombre(ejerciciosPruebas,"Numeros Random")).toEqual([]);
       });
     it("Devuelve la lista de coincidencia con el nombre del ejercicio", () => {
-        let catalogo=new Catalogo(ejerciciosPruebas);
-        expect(catalogo.buscarPorNombre("Numeros Romanos")).toEqual([{"categoria": "Numeros", "imagen": "10PUZY7ai9akJ5VAGzWbPNKGjQXBQ4-f9", "resumen": "resumen2", "titulo": "Numeros Romanos"}]);
+        expect(buscarPorNombre(ejerciciosPruebas,"Numeros Romanos")).toEqual([{"categoria": "Numeros", "imagen": "10PUZY7ai9akJ5VAGzWbPNKGjQXBQ4-f9", "resumen": "resumen2", "titulo": "Numeros Romanos"}]);
     });
     it("Devuelve la lista de coincidencia con el nombre del ejercicio", () => {
-        let catalogo=new Catalogo(ejerciciosPruebas);
-        expect(catalogo.buscarPorNombre("Numeros Primos")).toEqual([{"categoria": "Numeros", "imagen": "1euc7nmD5AGroYcxYYx4DEd-MYAzXuUph", "resumen": "resumen", "titulo": "Numeros Primos"}]);
+        expect(buscarPorNombre(ejerciciosPruebas,"Numeros Primos")).toEqual([{"categoria": "Numeros", "imagen": "1euc7nmD5AGroYcxYYx4DEd-MYAzXuUph", "resumen": "resumen", "titulo": "Numeros Primos"}]);
     });
     it("Devuelve la lista de coincidencia con el ejercicio titulado numeros random", () => {
-      let ejercicio=new Ejercicio("Numeros Random",undefined,undefined,undefined);
-      ejerciciosPruebas.push(ejercicio);
-      let catalogo=new Catalogo(ejerciciosPruebas);
-      expect(catalogo.buscarPorNombre("Numeros Random")).toEqual([{"categoria": "","imagen": "1euc7nmD5AGroYcxYYx4DEd-MYAzXuUph","resumen": "","titulo": "Numeros Random"}]);
-  });
+      ejerciciosPruebas.push(new Ejercicio("Numeros Random",undefined,undefined,undefined));
+      expect(buscarPorNombre(ejerciciosPruebas,"Numeros Random")).toEqual([{"categoria": "","imagen": "1euc7nmD5AGroYcxYYx4DEd-MYAzXuUph","resumen": "","titulo": "Numeros Random"}]);
+    });
     it("Devuelve la lista completa si no se ingresa un nombre para buscar", () => {
-      let catalogo=new Catalogo(ejerciciosPruebas);
-      expect(catalogo.buscarPorNombre("")).toEqual(ejerciciosPruebas);
-  });
+      expect(buscarPorNombre(ejerciciosPruebas,"")).toEqual(ejerciciosPruebas);
+    });
   }
 );
 
