9a9cb77b9b275e511bf528c5649c88fcd0ce405c
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-10-24 22:36:09 -0400

    Devuelve el username del usuario creado usuario juancho

--- src/crearUsuario.spec.js
+++ src/crearUsuario.spec.js
@@ -3,11 +3,18 @@ describe("Crear usuarios version basica", () => {
         let usuario=new Usuario("pepe");
         expect(usuario.getNombre()).toEqual("pepe");
       });
+      it("Devuelve el username del usuario creado usuario juancho", () => {
+        let usuario=new Usuario("juancho");
+        expect(usuario.getNombre()).toEqual("juancho");
+      });
     }
   );
 
   class Usuario{
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
         getNombre(){
-            return "pepe";
+            return this.nombre;
         }
   };
